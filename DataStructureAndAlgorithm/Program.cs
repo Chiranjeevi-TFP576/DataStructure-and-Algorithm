11b2f24452ddb71738c9c3855dd11c1013c4269c
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2022-11-19 21:34:23 +0530

    BinarySearch Word

---
+++ DataStructureAndAlgorithm/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructureAndAlgorithm;
+
+Console.WriteLine("Hello, World!");
+ 
+    static void Main(string[] args)
+    {
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("Select Programs\n 1.Binary search");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    BinarySearchWord_UC1 binarysearch = new BinarySearchWord_UC1();
+                    binarysearch.Search(@"D:\Bridgelabs\DataStructureAndAlgorithm\DataStructureAndAlgorithm\Word.txt");
+                    break;
+            }
+        }
+    }
